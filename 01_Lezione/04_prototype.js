32b8baa2775a8ccf2eee7a21703c02bc664870af
aeisolution <info@aeisolution.it>
2017-11-09 08:52:26 +0100

    Materiale per Lezione N.2

---
+++ 01_Lezione/04_prototype.js
@@ -0,0 +1,20 @@
+// 04 Prototype
+
+var Alunno = function(nome, cognome) {
+  this.nome = nome;
+  this.cognome = cognome;
+
+  //a. Creazione proprietà nominativo (concatena nome e cognome)
+
+}
+
+//b. Utilizzo prototype per definizione metodo
+
+
+// creazione oggetto alunno1 di tipo Alunno
+var alunno1
+
+
+// Stampa oggetto alunno1
+console.dir(alunno1)
+
